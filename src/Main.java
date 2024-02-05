3981439347d56a104c63faa6735b4c3f12845252
rival <rivaljyoung@gmail.com>
2024-02-05 17:36:22 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        double summerCost = 200.45;
+        double winterCost = 346.52;
+        double springCost = 120.74;
+        double fallCost = 75.86;
+
+        double totalCost = 0;
+
+        System.out.println("the summer maintenance cost is " + summerCost);
+        System.out.println("the winter maintenance cost is " + winterCost);
+        System.out.println("the spring maintenance cost is " + springCost);
+        System.out.println("the spring maintenance cost is " + fallCost);
+
+        totalCost = summerCost + winterCost + springCostWWW + fallCost;
+
+        System.out.println("the yearly cost of maintenance is " + totalCost);
+    }
+}
